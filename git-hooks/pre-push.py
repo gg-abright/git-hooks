b02a46bfe5253755bda1f2ac13366c1621f6f3bd
Adam Bright <abright@goguardian.com>
2023-09-06 16:44:40 -0400

    fixes

--- git-hooks/pre-push.py
+++ git-hooks/pre-push.py
@@ -21,18 +21,20 @@ def git_diff_remote(branch, remote="origin"):
     files = [f for f in diff_cmd.stdout.split("\n") if " | " in f]
     return list(map(lambda f: f.split("|")[0].strip(), files))
 
-def unique_directories(file_list):
-    return list(set(map(lambda f: f[:f.rfind("/")], file_list)))
+def get_directory(file):
+    return file[:file.rfind("/")]
 
-def find_file_in_parents(dir, filename, root=os.environ["PWD"], stop_at=os.environ["PWD"]):
+def find_file_in_parents(dir, filename, root=os.environ.get("PWD"), stop_at=os.environ.get("PWD")):
     full_filepath = "/".join([root, dir, filename])
-    print("full filepath", dir, filename)
 
     while not exists(full_filepath):
         dir = dir[:dir.rfind("/")]
-        full_filepath = "/".join([dir, filename])
+        full_filepath = "/".join([root, dir, filename])
 
-    return full_filepath
+    return full_filepath.replace("//", "/", -1)
+
+def unique_list(l):
+    return list(set(list(l)))
 
 # stash uncommitted changes
 git_stash()
@@ -42,13 +44,24 @@ branch = git_branch()
 
 # check changes being pushed
 files_changed = git_diff_remote(branch)
-directories = unique_directories(files_changed)
+directories = unique_list(map(lambda f: get_directory(f), files_changed))
 
 # find closest Makefile for each directory
-makefiles = list(set([map(lambda d: find_file_in_parents(d, "Makefile"), directories)]))
-print(makefiles)
+makefiles = unique_list(map(lambda d: find_file_in_parents(d, "Makefile"), directories))
 
 # run `make test` on each unique Makefile and fail push if tests fail
+for makefile in makefiles:
+    # verify `test` target exists
+
+    # get CWD for `make test` to be run in
+    dir = get_directory(makefile)
+
+    try:
+        subprocess.run(["make", "test"], cwd=dir)
+    except:
+        print(f"Tests failed running `make test` in `{dir}`")
+        git_unstash()
+        exit(1)
 
 # apply stash to restore uncommitted changes
 git_unstash()
