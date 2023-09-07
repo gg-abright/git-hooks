4045a1ca8e023adb0ad856e285a8923c7393a391
Adam Bright <abright@goguardian.com>
2023-09-06 22:48:45 -0400

    add formatting option with commit and finalize test run

--- git-hooks/pre-push.py
+++ git-hooks/pre-push.py
@@ -21,6 +21,17 @@ def git_diff_remote(branch, remote="origin"):
     files = [f for f in diff_cmd.stdout.split("\n") if " | " in f]
     return list(map(lambda f: f.split("|")[0].strip(), files))
 
+def git_modified_files():
+    status_cmd = subprocess.run(["git", "status"], text=True, capture_output=True)
+    modified = [f for f in status_cmd.stdout.split("\n") if "modified:" in f]
+    return list(map(lambda f: f.split(":")[1].strip(), modified))
+
+def git_add(files):
+    subprocess.run(["git", "status"] + files)
+
+def git_commit(msg):
+    subprocess.run(["git", "commit", "-m", f"\"{msg}\""])
+
 def get_directory(file):
     return file[:file.rfind("/")]
 
@@ -36,6 +47,22 @@ def find_file_in_parents(dir, filename, root=os.environ.get("PWD"), stop_at=os.e
 def unique_list(l):
     return list(set(list(l)))
 
+def a_line_starts_with(filepath, prefix):
+    with open(filepath) as f:
+        for line in f:
+            if line.startswith(prefix):
+                return True
+
+def run_make_target(makefilepath, target):
+    # verify `test` target exists
+    if not a_line_starts_with(makefilepath, f"{target}:"):
+        return None
+
+    # get CWD for `make <target>` to be run in
+    dir = get_directory(makefilepath)
+
+    return subprocess.run(["make", target], cwd=dir)
+
 # stash uncommitted changes
 git_stash()
 
@@ -49,16 +76,18 @@ directories = unique_list(map(lambda f: get_directory(f), files_changed))
 # find closest Makefile for each directory
 makefiles = unique_list(map(lambda d: find_file_in_parents(d, "Makefile"), directories))
 
-# run `make test` on each unique Makefile and fail push if tests fail
+# run `make test` and `make format` on each unique Makefile and fail push if tests fail
 for makefile in makefiles:
-    # verify `test` target exists
+    format_result = run_make_target(makefile, "format")
+    modified = git_modified_files()
 
-    # get CWD for `make test` to be run in
-    dir = get_directory(makefile)
+    if format_result is not None and len(modified) > 0:
+        git_add(modified)
+        git_commit("formatting")
 
-    try:
-        subprocess.run(["make", "test"], cwd=dir)
-    except:
+    test_result = run_make_target(makefile, "test")
+
+    if test_result is not None and test_result.returncode is not 0:
         print(f"Tests failed running `make test` in `{dir}`")
         git_unstash()
         exit(1)
